c4646fb9a81993a263f2b1d0bfcab1b9480c2bb6
Simon Taite <simontaite1@gmail.com>
2017-03-14 14:20:11 +0000

    Test repo, extneding IdentityUserRole with column. Index applied to column

---
+++ Data/Migrations/20170314141857_AddCustomPropertyWithIndex.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExtendIdentityUserRole.Data.Migrations
+{
+    public partial class AddCustomPropertyWithIndex : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUserRoles",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomProperty",
+                table: "AspNetUserRoles",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_CustomProperty",
+                table: "AspNetUserRoles",
+                column: "CustomProperty");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUserRoles_CustomProperty",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "CustomProperty",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName");
+        }
+    }
+}
